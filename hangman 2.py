e54008e5aeb0c741d33640b4ee27f2a0087fbaaa
Goofy Goober <91341620+MB-KiddA@users.noreply.github.com>
2023-06-07 10:56:06 -0700

    Hangman 1.0

--- hangman 2.py
+++ hangman 2.py
@@ -1,6 +1,70 @@
 import random, time
 from words import words
 import string
+import os
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+def Retry():
+    retry = input('Play Again? (Y/N): ')
+    retry = str(retry.upper())
+    if retry == 'Y':
+        os.system('cls')
+        hangman()
+    if retry == 'N':
+        SystemExit
+    else:
+        os.system('cls')
+        Retry
 
 def s_print(string, letter_speed = 0.01):
     for character in string:
@@ -23,14 +87,16 @@ def hangman():
     # letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
-
-    lives = 7
+    lives = 6
+    pic = 0
 
     # getting user input
     while len(word_letters) > 0 and lives > 0:
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        os.system('cls')
         s_print(f"You have {lives} lives left and you have used these letters: {' '.join(used_letters)}")
+        e_print(HANGMANPICS[pic])
         # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print([lives])
@@ -45,6 +111,9 @@ def hangman():
 
             else:
                 lives = lives - 1  # takes away a life if wrong
+                pic = pic + 1
+                os.system('cls')
+
                 e_print(F"\nYour letter {user_letter} is not in the word.")
 
         elif user_letter in used_letters:
@@ -55,9 +124,11 @@ def hangman():
 
     # gets here when len(word_letters) == 0 OR when lives == 0
     if lives == 0:
-        s_print([lives])
-        s_print(f"YAY! You guessed the word' {word} '!!")
-
+        os.system('cls')
+        e_print(HANGMANPICS[6])
+        s_print(f"\nOOF You Lost! The Word is:' {word} '!!")
+        Retry()
+        
 
 if __name__ == '__main__':
     hangman()
