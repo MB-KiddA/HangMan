63d92f422ee5d6c9d830c5d24eaf013679b78fc7
Goofy Goober <91341620+MB-KiddA@users.noreply.github.com>
2023-06-07 10:57:06 -0700

    Hangman 1.1

--- Hangman.py
+++ Hangman.py
@@ -1,35 +1,134 @@
-import random
+import random, time
 from words import words
+import string
+import os
 
-def get_vaild_words(words):
-    word = random.choice(words)
-    while '-' in word or ' ' in word:
-        word = random.choice(words)
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
+def Retry():
+    retry = input('Play Again? (Y/N): ')
+    retry = str(retry.upper())
+    if retry == 'Y':
+        os.system('cls')
+        hangman()
+    if retry == 'N':
+        SystemExit
+    else:
+        os.system('cls')
+        Retry
 
+def s_print(string, letter_speed = 0.01):
+    for character in string:
+        print(f"{character}",end="",flush=True)
+        time.sleep(letter_speed)
+
+def e_print(string, color = 36, letter_speed = 0.001):
+    for character in string:
+        print(f"{character}",end="",flush=True)
+        time.sleep(letter_speed)
+
+def get_valid_word(words):
+    word = random.choice(list(set(words)-{"kwantlen", "rai", "zimmerman"}))
+    # randomly chooses something from the list
     return word.upper()
 
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word)
-    alphabet = set(str.ascii_uppercase)
-    used_letters = set()
+    # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+    lives = 6
+    pic = 0
 
-    while len(word_letters) > 0:
-        print('you have used these letters: ' ', ' ' '.join(used.letters))
+    # getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        os.system('cls')
+        s_print(f"You have {lives} lives left and you have used these letters: {' '.join(used_letters)}")
+        e_print(HANGMANPICS[pic])
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print([lives])
+        print('Current word: ', ' '.join(word_list))
 
-    word_list = [letter if letter in used_letters else '-' for letter in word]
-    print('current word: ', ' '.join(word_list))
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
 
+            else:
+                lives = lives - 1  # takes away a life if wrong
+                pic = pic + 1
+                os.system('cls')
 
-    user_letter = input('guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letter.add(user_letter)
-        if user_letter in word_letters:
-            word.letters.remove(user_letter)
-
+                e_print(F"\nYour letter {user_letter} is not in the word.")
 
         elif user_letter in used_letters:
-            print ('you have already used that character. Please try again.')
+            e_print('\nYou have already used that letter. Guess another letter.')
 
-            user_input = input
+        else:
+            e_print('\nThat is not a valid letter.')
+
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        os.system('cls')
+        e_print(HANGMANPICS[6])
+        s_print(f"\nOOF You Lost! The Word is:' {word} '!!")
+        Retry()
+        
+
+if __name__ == '__main__':
+    hangman()
